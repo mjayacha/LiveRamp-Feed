63173d38f67958bb02c4cf85e6aee82d8f1b480c
mjayacha <jcmounika91@gmail.com>
2019-10-31 14:22:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,45 @@ import os
 import gzip
 import sftp_utils as sftp
 import paramiko
-
+from pathlib import Path
+from Creds import liveramp_feed
 
 def data_extract_to_csv(query, username, local_dir_fq, use_pyodbc=True):
     DF = DBUtilities.query_data_return_pandas_df(query, username , use_pyodbc)
     DF.to_csv(local_dir_fq,header=True, index =False, compression = 'gzip')
     print("Database file successfully exported")
 
-def data_transfer_to_sftp_client(user_name, host_name, key_file, local_dir_fq, remote_dir_fq ):
-    with sftp.SFTPCon(user_name, host_name, pkey_file=key_file) as sftp_con:
+def data_transfer_to_sftp_client(username, hostname, password, local_dir_fq, remote_dir_fq):
+
+    with sftp.SFTPCon(username, hostname, password) as sftp_con:
         sftp_con.put(local_dir_fq, remote_dir_fq)
     print("Database file successfully exported to SFTP client")
 
-def main(query, username, AWS_user_name, AWS_host_name, remote_dir_fq, key_file, local_dir_fq):
+def main():
     try:
+        file_name = 'liveramp_file.csv.gz'
+        local_dir_fq = os.path.join(Path().absolute(), file_name)
+        remote_dir_fq = '/uploads/build_com_onboarding/liveramp_file.csv.gz'
+        liveramp = liveramp_feed()
+        username = liveramp.username
+        hostname = liveramp.hostname
+        password = liveramp.password
+        query = 'select 1'
+        username = 'Reporter'
         data_extract_to_csv(query, username,local_dir_fq)
-        data_transfer_to_sftp_client(AWS_user_name, AWS_host_name, key_file, local_dir_fq, remote_dir_fq)
+
+        data_transfer_to_sftp_client(username, hostname, password, local_dir_fq, remote_dir_fq)
         return True
     except Exception as e:
         raise
 
     finally:
-        print("Removing .csv files")
-        try:
-            os.remove(local_dir_fq )
-        except WindowsError as e:
-            pass
+            try:
+                os.remove(local_dir_fq)
+            except WindowsError as e:
+                pass
+
+if __name__ == '__main__':
+
+    main()
+    print("Script completed.")
