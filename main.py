e6ca8b56dad5392e690800d59c95674cb26e85db
mjayacha <jcmounika91@gmail.com>
2019-11-04 15:00:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ import paramiko
 from pathlib import Path
 from Creds import liveramp_feed
 
+file_name = 'liveramp_file.csv.gz'
+local_dir_fq = os.path.join(Path().absolute(), file_name)
+remote_dir_fq = '/uploads/build_com_onboarding/liveramp_file.csv.gz'
+liveramp = liveramp_feed()
+username = liveramp.username
+hostname = liveramp.hostname
+password = liveramp.password
+query = 'select * from  [dw].[liveRampFeed](default)'
+username = 'Reporter'
+
 def data_extract_to_csv(query, username, local_dir_fq, use_pyodbc=True):
     DF = DBUtilities.query_data_return_pandas_df(query, username , use_pyodbc)
     DF.to_csv(local_dir_fq,header=True, index =False, compression = 'gzip')
@@ -21,29 +31,18 @@ def data_transfer_to_sftp_client(username, hostname, password, local_dir_fq, rem
 
 def main():
     try:
-        file_name = 'liveramp_file.csv.gz'
-        local_dir_fq = os.path.join(Path().absolute(), file_name)
-        remote_dir_fq = '/uploads/build_com_onboarding/liveramp_file.csv.gz'
-        liveramp = liveramp_feed()
-        username = liveramp.username
-        hostname = liveramp.hostname
-        password = liveramp.password
-        query = 'select * from [datamart].[dw].[liveRamp]'
-        username = 'Reporter'
-        data_extract_to_csv(query, username,local_dir_fq)
-
-        data_transfer_to_sftp_client(username, hostname, password, local_dir_fq, remote_dir_fq)
-        return True
+      data_extract_to_csv(query, username,local_dir_fq)
+      data_transfer_to_sftp_client(username, hostname, password, local_dir_fq, remote_dir_fq)
     except Exception as e:
         raise
 
     finally:
-            try:
-                os.remove(local_dir_fq)
-            except WindowsError as e:
-                pass
+        try:
+            os.remove(local_dir_fq)
+        except WindowsError as e:
+            pass
 
 if __name__ == '__main__':
+        main()
+        print("Script completed.")
 
-    main()
-    print("Script completed.")
